931b50635c6c137af0e62994e3ebdaef8d499a63
“ingecar” <j.cardonao@uniandes.edu.co>
2023-09-25 15:10:16 -0500

    nuevo commit

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,5 @@
 const username = 'John Doe';
 const age = 26;
 const hasHobbies = 'Dancing, Singing, Reading';
+// practice
 console.log(`Hello, my name is ${username} and I am ${age} years old and I have some hobbies like ${hasHobbies}.`);
