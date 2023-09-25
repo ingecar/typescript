b3f21944099cee4ce9831da5d359d44e012bc467
“ingecar” <j.cardonao@uniandes.edu.co>
2023-09-25 14:39:42 -0500

    commit inicial

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const username = 'John Doe';
+const age = 26;
+const hasHobbies = 'Dancing, Singing, Reading';
+console.log(`Hello, my name is ${username} and I am ${age} years old and I have some hobbies like ${hasHobbies}.`);
