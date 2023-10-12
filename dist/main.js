e7f9fabd8b6e0f531e951e3e21ff0884cfe0b097
“ingecar” <j.cardonao@uniandes.edu.co>
2023-10-12 18:17:38 -0500

    Front aprendiz

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,9 @@
 import { Aprendiz, NivelEducativo } from "./aprendiz.js";
 export var ap = new Aprendiz('Andres', 'Herrera', 'avatar.png', 29, NivelEducativo.POSGRADO);
-console.log(ap);
+var aprendizTable = document.getElementById("aprendiz");
+mostrarAprendiz(ap);
+function mostrarAprendiz(aprendiz) {
+    var tbodyAprendiz = document.createElement('tbody');
+    tbodyAprendiz.innerHTML = "  <tr><td colspan=2><img src=\"./".concat(aprendiz.avatar, "\" height=\"100\"></td></tr>\n                                 <tr><td>Nombres:</td><td>").concat(aprendiz.nombres, "</td></tr>\n                                 <tr><td>Apellidos:</td><td>").concat(aprendiz.apellidos, "</td></tr>\n                                 <tr><td>Nivel Educativo:</td><td>").concat(aprendiz.nivelEducativo, "</td></tr>        \n                                 <tr><td>Edad:</td><td>").concat(aprendiz.edad, "</td></tr>");
+    aprendizTable.appendChild(tbodyAprendiz);
+}
